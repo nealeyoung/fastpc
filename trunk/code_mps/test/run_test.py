4bd220d0379952cd4a2fa63d42e0f469bb8825f3
monikkhare <monikkhare@e2c49934-d751-0410-bf5a-0de24663539b>
2008-08-18 22:48:08 +0000

    Fixed GLPK input file creation and some other minor changes.

--- trunk/code_mps/test/run_test.py
+++ trunk/code_mps/test/run_test.py
@@ -47,26 +47,39 @@ def create_input_file(r, c, non_zero, lower, upper, file_name):
             glpk_file.write(item)
         glpk_file.write(row[-1][:-1] + ' < 1 \n')
     glpk_file.write('Bounds \n')
+    for x in range( c):
+        glpk_file.write('0 < var' +str(x)+ ' \n')
     glpk_file.write('End \n')
   
 def main():
 
+    #total number of input files to be generated
     max_inputs = 2
+    #total number of runs to be done for each input file generated
     max_runs_per_input = 2
 
+    #no of rows, r, is in the range [row_min, row_max]
     row_min = 30
     row_max = 100
+    #no of columns, c, is in the range [col_min, col_max]    
     col_min = 30
     col_max = 100
+
+    #total number of non zero's = f*r*c (r = rows, c = columns) where f is in the range [fraction_nonzero_low, fraction_nonzero_high]
     fraction_nonzero_low = 0.1
     fraction_nonzero_high = 0.9
-    max_lower = 0
+
+    #the range for coefficients in an input are in range [a,b] such that min_lower <= a <= b <= max_upper
+    min_lower = 0
     max_upper = 50
 
+    #eps for a run is in range [eps_low, eps_high]
     eps_low = 0.005
     eps_high = 0.1
 
-    sort_ratio = 1 #make this random (1 or 2) to test both exact and sudo sorting
+    #each run is done with sort_ratio 1 (exact sorting)
+    #and also for the sort_ratio defined here (approximate sorting)
+    a_sort_ratio = 2 #if set to 0 or 1, only exact sorting run will be done
 
     input_file_prefix = 'input_test_m'
     output_file_name = 'output_test_m'
@@ -80,19 +93,18 @@ def main():
         test_col = random.randint(col_min, col_max)
         test_total = test_row*test_col
         test_nonzero = random.randint(math.ceil(fraction_nonzero_low*test_total), math.floor(fraction_nonzero_high*test_total))
-        test_lower = random.randint(max_lower, math.ceil((max_lower+max_upper)/2))
+        test_lower = random.randint(min_lower, math.ceil((min_lower+max_upper)/2))
         test_upper = random.randint(test_lower, max_upper)
-        if (test_upper == test_lower):
-            test_upper = test_upper + 1
+        if (test_upper == 0):
+            test_upper = 1
         test_input_file = input_file_prefix + "_"  + str(i)
 
-        #record the parameters for the input file
+        #record the parameters for the input file in the input_config_file
         print "Input file name: ", test_input_file
         print "Rows : ", test_row
         print "Columns : ", test_col
         print "Coefficient lower bound: ", test_lower
         print "Coefficient upper bound: ", test_upper, "\n"
-        #write_input_config(test_row, test_col, test_nonzero, test_lower, test_upper, test_input_file)
 
         #create input file
         create_input_file(test_row, test_col, test_nonzero, test_lower, test_upper, test_input_file)
@@ -102,9 +114,14 @@ def main():
             #run the test and store the output
             test_eps = random.uniform(eps_low, eps_high)
 
-            cmd = "../fastpc" + ' ' + str(test_eps) + ' ' + test_input_file + ' ' + str(sort_ratio) + " >> " + output_file_name
-
+            #run with sort_ratio = 1 (exact sorting)
+            cmd = "../fastpc" + ' ' + str(test_eps) + ' ' + test_input_file + " 1 >> " + output_file_name
             os.system(cmd)
+
+            if (a_sort_ratio > 1) :
+                #run with sort_ration > 1 (approx sorting)
+                cmd = "../fastpc" + ' ' + str(test_eps) + ' ' + test_input_file + ' ' + str(a_sort_ratio) + " >> " + output_file_name
+                os.system(cmd)
             
     output_file.close()
 
