baa5f92e2a7e2eaf924d5f31d02706275ced490b
cole.steve <cole.steve@e2c49934-d751-0410-bf5a-0de24663539b>
2008-08-20 19:51:37 +0000

    Improved meta-script.

--- code_mps/test_mps/do_all.sh
+++ code_mps/test_mps/do_all.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+  echo "Usage: $0 fileprefix"
+  exit 1
+fi
 python make_tests.py $1
+echo 'Done creating tests'
 python run_tests.py $1
+echo 'Done running tests'
 python parse_output.py $1
 
