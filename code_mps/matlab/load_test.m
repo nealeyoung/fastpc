403f9d3abf436783a0508ed898bf29b65fd4ab4e
cole.steve <cole.steve@e2c49934-d751-0410-bf5a-0de24663539b>
2008-09-05 16:23:36 +0000

    Commented matlab file.

--- code_mps/matlab/load_test.m
+++ code_mps/matlab/load_test.m
@@ -33,14 +33,15 @@ for i=1:size_x
 end
 
 
-
+%set up lsf problem: (M'M)c = M'y 
+%    => for each row i, (M'M)(i)/(M'y)(i)*c <= 1; solve for c
 M = M*M.';
 y = M.'*y;
 for k=1:size_x
 	M(k,:) = M(k,:)/y(k);
 end
 
-
+%output constraints in fastpc format
 ofid = fopen(out_file,'w');
 fprintf(ofid,'%d %d %d\n',[size_x,size_funcs,size_x*size_funcs]);
 for i=1:size_x
@@ -51,15 +52,17 @@ for i=1:size_x
 end
 fclose(ofid);
 
+%run fastpc
 system('../fastpc .05 fastpc_input_matlab');
 load tomog_solution
 
-
+%build solution function as linear combo of input functions
 big_func = @(x)0
 for i=1:size_funcs
 	big_func = @(x)tomog_solution(i)*funcs{i}(x) + big_func(x);
 end
 
+%graph solution function and original points
 %x_cor=0:0.5:10;
 x_cor = x;
 x
