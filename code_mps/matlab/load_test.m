09422686b6bf6a6e2c99cd85c8cd1017c575a20e
plonjers <plonjers@e2c49934-d751-0410-bf5a-0de24663539b>
2008-09-05 05:30:33 +0000

    made it better

--- code_mps/matlab/load_test.m
+++ code_mps/matlab/load_test.m
@@ -4,70 +4,65 @@ out_file = 'fastpc_input_matlab'
 load my_xy.txt
 
 x = my_xy(:,1);
-y = my_xy(:,2)
+y = my_xy(:,2);
+y_org = y;
 size_x_temp = size(x);
 size_x = size_x_temp(1);
 
 %read functions
 fid=fopen('input');
-a = {};
-size_a = 0;
+funcs = {};
+size_funcs = 0;
 
 %build cell array of function defs
 while 1
     tline = fgetl(fid);
     if ~ischar(tline),   break,   end
-    a= [a tline];
-    size_a = size_a + 1;
+      funcs{size_funcs +1}= @(x)(eval(tline));
+    size_funcs = size_funcs + 1;
 end
 fclose(fid);
 
-M = zeros(size_x, size_a);
+M = zeros(size_x, size_funcs);
 
 %evaluate functions for each x value
 for i=1:size_x
-    for j=1:size_a
-        temp=@(x) eval(a{j});
-        M(i,j) = temp(x(i));
+    for j=1:size_funcs
+       M(i,j) = funcs{j}(x(i));
     end
 end
 
 
 
-M = M*M.'
-y = M.'*y
+M = M*M.';
+y = M.'*y;
 for k=1:size_x
-    M(k,:) = M(k,:)/y(k)
+	M(k,:) = M(k,:)/y(k);
 end
 
-num2str(size_x)
-num2str(size_a)
+
 ofid = fopen(out_file,'w');
-fprintf(ofid,'%d %d %d\n',[size_x,size_a,size_x*size_a]);
+fprintf(ofid,'%d %d %d\n',[size_x,size_funcs,size_x*size_funcs]);
 for i=1:size_x
-    for j=1:size_a
-        M(i,j)
+    for j=1:size_funcs
+	    M(i,j);
         fprintf(ofid,'%d %d %f\n', [i-1, j-1, M(i,j)]);
     end
 end
 fclose(ofid);
 
-%bang bang
-!../Desktop/summerResearch08/fastpc/code_mps/fastpc .05 fastpc_input_matlab
+system('../fastpc .05 fastpc_input_matlab');
 load tomog_solution
 
-%plot new points
-big_fcn = ''
-big_fcn = strcat(num2str(tomog_solution(1)), '*(', a(1), ')')
-for j=2:size_a
-   big_fcn = strcat(big_fcn, '+', num2str(tomog_solution(j)), '*(', a(j), ')')
+
+big_func = @(x)0
+for i=1:size_funcs
+	big_func = @(x)tomog_solution(i)*funcs{i}(x) + big_func(x);
 end
-x_coord=0:0.5:10;
-final = @(x)eval(big_fcn{1});
-%plot(x_coord,final(x_coord))
+
+%x_cor=0:0.5:10;
+x_cor = x;
 x
-y
-plot(x_coord,final(x_coord),x,y,'--rs','LineWidth',2,...
-                'MarkerEdgeColor','k',...
-                'MarkerFaceColor','g',...
-                'MarkerSize',10)
+y_org
+y_cor=big_func(x_cor);
+plot(x_cor,y_cor,'r',x,y_org,'g')
