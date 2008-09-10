64f2a22838250db69366a15bc467e42d983cc49d
cole.steve <cole.steve@e2c49934-d751-0410-bf5a-0de24663539b>
2008-09-10 23:52:22 +0000

    Modified do_all to include tomog tests.

--- code_mps/test_mps/do_all.sh
+++ code_mps/test_mps/do_all.sh
@@ -5,11 +5,22 @@ then
   echo "Usage: $0 fileprefix"
   exit 1
 fi
-python make_tests.py $1
-echo 'Done creating tests'
-python tomog/scan_all.py $1 test_cases test_cases_glpk 0.01 2
-echo 'Done making tomog tests'
+echo -n 'Make random tests (y or n)?'
+read $random
+echo -n 'Make tomog tests (y or n)?'
+read $tomog
+if [ "$random" = "y" ]
+  echo 'Creating random tests...'
+  python make_tests.py $1
+  echo 'Done creating random tests'
+fi
+if [ "$tomog" = "y" ]
+  echo 'Creating tomog tests...'
+  python tomog/scan_all.py $1 test_cases test_cases_glpk 0.01 2
+  echo 'Done making tomog tests'
+fi
 python run_tests.py $1
 echo 'Done running tests'
 python parse_output.py $1
+echo 'Output parsed'
 
