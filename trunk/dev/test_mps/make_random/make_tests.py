a9850e2463173e7969e451386cb4537d00f45f5b
monikkhare <monikkhare@e2c49934-d751-0410-bf5a-0de24663539b>
2009-06-18 01:12:47 +0000

    Updated parameters to create tests.

--- trunk/dev/test_mps/make_random/make_tests.py
+++ trunk/dev/test_mps/make_random/make_tests.py
@@ -6,31 +6,33 @@ def main():
 
     #------- INPUT FILE PARAMETERS - BEGIN -------------
     # Total number of input sets to be generated (each set has dif num of rows/cols)
-    input_sets = 12
+    input_sets = 10
     
     # Number of rows, r, starts in the range 10* [row_min, row_max] and increases by factor of 10 for each run
-    row_min = 500
-    row_max = 550
+    row_min = 1000
+    row_max = 1400
+
     # Number of columns, c, is in the range [col_min, col_max]    
-    col_min = 900
-    col_max = 950
+    col_min = 3500
+    col_max = 3700
+
     # Increment
-    row_increment = 800
-    col_increment = 400
+    row_increment = 1500
+    col_increment = 1000
+
+    # Create by density
+    create_by_density = True
 
     # Densities for each matrix size
-    # These will be considered only when the sparse option below is set to 'False'
-    #densities = [0.01, 0.1, 0.4]
-    densities = [0.1, 0.5]
+    # These will be considered only when the create_by_density is set to 'True'
+    densities = [0.1, 0.4]
     
     # The range for coefficients in an input are in range [min_lower,max_upper]
     lower = 0
     upper = 100
 
     # Turn this option to 'True' to generate very sparse matrices with a constant number of non-zeroes in each row
-    # This method is more suitable for reasonably large matrices
-    # If you turn this option on, the denisities defined above will be ignored
-    # SHIFT THIS OPTION TO ARGUMENTS!!
+    # This method is more suitable for reasonably large and sparse matrices
     sparse = True
 
     # Number of non zeroes in each row
@@ -56,16 +58,19 @@ def main():
     pathname = full_pathname[:full_pathname.rfind('/')+1]
 
     for i in range(input_sets):
-        #set up parameters for the test
+
+        if create_by_density:
+            file_prefix = input_file_prefix + '_d'
+            for d in densities: #create input file
+                cmd_create = 'python ' + pathname + 'generate_input.py ' + ' ' + str(test_row) + ' ' + str(test_col) + ' ' + str(d) + ' ' + str(lower) + ' ' + str(upper) + ' ' + file_prefix
+                os.system(cmd_create)
+        if sparse:
+            file_prefix = input_file_prefix + '_s'
+            cmd_create = 'python ' + pathname + 'generate_sparse_input.py ' + ' ' + str(test_row) + ' ' + str(test_col) + ' ' + str(max_per_row) + ' ' + str(lower) + ' ' + str(upper) + ' ' + file_prefix
+            os.system(cmd_create)
+
+        # Update rows and columns for next set
         test_row += row_increment
         test_col += col_increment
-
-        if not sparse:
-            for d in densities: #create input file
-                cmd_create = 'python ' + pathname + 'generate_input.py ' + ' ' + str(test_row) + ' ' + str(test_col) + ' ' + str(d) + ' ' + str(lower) + ' ' + str(upper) + ' ' + input_file_prefix
-                os.system(cmd_create)
-        else:
-            cmd_create = 'python ' + pathname + 'generate_sparse_input.py ' + ' ' + str(test_row) + ' ' + str(test_col) + ' ' + str(max_per_row) + ' ' + str(lower) + ' ' + str(upper) + ' ' + input_file_prefix
-            os.system(cmd_create)
             
 main()
